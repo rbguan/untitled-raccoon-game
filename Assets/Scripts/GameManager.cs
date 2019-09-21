5855eb65d7ad1881ffafc8b00c230af76eb2c9c4
Ali Kedwaii <ali.kedwaii@utexas.edu>
2019-09-21 15:38:19 -0500

    GameManager init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private enum Turn {
+        HUMAN,
+        RACCOON
+    }
+
+    // Whose turn it is
+    private Turn turn = Turn.HUMAN;
+
+    [SerializeField] private GameObject MainCamera;
+    [SerializeField] private GameObject HumanPlayer;
+    [SerializeField] private GameObject RaccoonPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SwitchTurn() {
+        turn = (turn == Turn.HUMAN) ? Turn.RACCOON : Turn.HUMAN;
+        CameraSwitch();
+    }
+
+    private void CameraSwitch() {
+        
+    }
+
+    //TODO
+    //Camera switch on turn switch
+    //Check win
+    //Turn coroutine
+
+    private IEnumerator GameLoop()
+    {
+        yield return StartCoroutine(TurnProgress());
+
+        if (m_GameWinner != null)
+        {
+            //reset
+        }
+        else
+        {
+            StartCoroutine(GameLoop());
+        }
+    }
+
+    private IEnumerator TurnProgress() {
+        if (turn == Turn.HUMAN){
+            //check walk and interact
+            //switch
+        }
+        else {
+            //check walk
+            //switch
+        }
+    }
+}
