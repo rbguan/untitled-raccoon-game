854b735eeccf61ec17e9a32e272d12d8abd37fc4
rguan <guan.robert@utexas.edu>
2019-09-21 14:13:05 -0500

    working on slider for walk energy

---
+++ Assets/Scripts/StatSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatSlider : MonoBehaviour
+{
+    public float StartingLevel;
+    public Slider mySlider;
+    public Image fillImage;
+
+    private void Awake()
+    {
+        mySlider.value = StartingLevel;
+        mySlider.minValue = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeLevel(float change)
+    {
+        mySlider.value += change;
+    }
+}
