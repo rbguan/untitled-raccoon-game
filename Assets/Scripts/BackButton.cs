80909a98a6edbef60628cbb730c9f24afe2cc192
Courtney <qwertyhwin@gmail.com>
2019-09-21 15:33:03 -0500

    Start screen linking with placeholder pages

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    /* TODO: make this more reusable by keeping track of previous scene */ 
+    public void GoToPreviousScene () {
+        SceneManager.LoadScene(0); // switches to the start screen
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    /* Switch to the MainGame scene by moving to the next scene in the queue. 
+    The scene queue can be found in File > Build Settings */
     public void PlayGame () {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /* Switch to scene "How To Play" by name */
+    public void ShowHowToPlay() {
+        SceneManager.LoadScene("How To Play");
+    }
+
+    /* Switch to scene "How To Play" by name */
+    public void ShowCredits() {
+        SceneManager.LoadScene("Credits");
     }
 }
