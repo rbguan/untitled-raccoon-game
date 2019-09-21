01746293207ec6fa101c379e7726f88f3a273101
rguan <guan.robert@utexas.edu>
2019-09-21 11:01:07 -0500

    some basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float InitialWalkRange;
+    [SerializeField] private float ParanoiaLevel;
+    [SerializeField] private float WalkRemaining;
+    [SerializeField] private bool DoneAction;
+    [SerializeField] private float WalkSpeed;
+    private Rigidbody myRigidBody;
+    private float VerticalInput;
+    private float HorizontalInput;
+
+    void Awake()
+    {
+        // InitialWalkRange = 30.0f;
+        // ParanoiaLevel = 1.0f;
+        // WalkRemaining = InitialWalkRange * ParanoiaLevel;
+        // DoneAction = false;
+        myRigidBody = GetComponent<Rigidbody>();
+        // WalkSpeed = 5.0f;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        VerticalInput = Input.GetAxis("Vertical");
+        HorizontalInput = Input.GetAxis("Horizontal");
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(WalkRemaining > 0)
+        {
+            Vector3 moveVector = transform.forward * VerticalInput * WalkSpeed * Time.deltaTime;
+            myRigidBody.MovePosition(myRigidBody.position + moveVector);
+
+            Vector3 sideVector = transform.right * HorizontalInput * WalkSpeed * Time.deltaTime;
+            myRigidBody.MovePosition(myRigidBody.position + sideVector);
+
+            WalkRemaining -= (sideVector.magnitude + moveVector.magnitude);
+        }
+        
+        
+    }
+
+    void OnMouseClick(){
+
+    }
+}
