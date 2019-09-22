3e569a9408763b4302e8df8ed5ce1c1207b88cba
Courtney <qwertyhwin@gmail.com>
2019-09-21 21:41:16 -0500

    multi-page how to menu implemented

---
+++ Assets/Scripts/HowToPlayMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlayMenu : MonoBehaviour
+{
+    public GameObject[] pages;
+    public int current = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pages[0].SetActive(true);
+        for(int i = 1; i < pages.Length; i++) {
+            pages[i].SetActive(false);
+        }
+    }
+
+    public void Forward() {
+        if(current < pages.Length -1) {
+            pages[current].SetActive(false);
+            current++;
+            pages[current].SetActive(true);
+        }
+    }
+
+    public void Back() {
+        if(current > 0) {
+            pages[current].SetActive(false);
+            current--;
+            pages[current].SetActive(true);
+        }
+    }
+}
