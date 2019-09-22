f00b589aa541e06a6138ce2671bd8515170f7e46
Courtney <qwertyhwin@gmail.com>
2019-09-22 11:36:16 -0500

    fix popup menu

---
+++ Assets/Scripts/PopupMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PopupMenu : MonoBehaviour
+{
+    public void ShowStartScreen () {
+        SceneManager.LoadScene(0);
+    }
+
+    /* Not working: Try to switch to scene "How To Play" without clearing the MainGame scene */
+    public void ShowHowToPlay() {
+        Scene mainGameScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene("How To Play", LoadSceneMode.Additive);
+    }
+}
