27363dbc3ae3b221f06d6b895068eb870f3e1647
TINH3AD <tinh3ad@gmail.com>
2019-09-22 04:56:41 -0500

    Barricades

---
+++ Assets/Scripts/Barricades.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barricades : InteractableObject
+{
+    [SerializeField] private BoxCollider blocker;
+    [SerializeField] private MeshRenderer render;
+    [SerializeField] private MeshRenderer render2;
+    public override void Awake()
+    {
+        //Debug.Log("subclass");
+        base.Awake();
+        blocker.enabled = true;
+        render.enabled = true;
+        render2.enabled = true;
+    }
+
+    public override void DoAction(){
+        Debug.Log("doing thing");
+        base.DoAction();
+        blocker.enabled = false;
+        render.enabled = false;
+        render2.enabled = false;
+    }
+}
+
